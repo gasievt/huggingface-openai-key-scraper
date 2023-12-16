94936a4730809a4008d867bfd5a213b4c934eb7d
gasievt <gasievt@inbox.ru>
2023-12-16 22:40:03 +0300

    changed validating loop

--- validate_keys.php
+++ validate_keys.php
@@ -1,19 +1,19 @@
 <?php
     include "./functions.php";
+    $key = explode('--key=', $argv[1])[1];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/v1/chat/completions");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    $headers = [
+        "Content-Type: application/json",
+        "Authorization: Bearer $key"
+    ];
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     foreach($models as $model){
-        $key = explode('--key=', $argv[1])[1];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/v1/chat/completions");
-        curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS,
             json_encode(['model'=>$model,
              'messages'=>[['role'=>'user', 'content'=>'Hi']],
              'temperature'=>0.7]));
-        $headers = [
-            "Content-Type: application/json",
-            "Authorization: Bearer $key"
-        ];
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_exec($ch);
         if(isValidKey($ch)){
             $fd = fopen("$model.txt", 'a+');
